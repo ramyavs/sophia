56ba8d3ce05481fcfb7170420754ba1330f5692d
ramyavs <31505005+ramyavs@users.noreply.github.com>
2018-01-11 08:06:35 -0300

    Create gh.c

---
+++ gh.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+void main()
+{
+int num1,num2.num3;
+printf("enter the number");
+scanf("%d%d%d",&num1,&num2,&num3);
+if(num1>num2)
+{
+if(num1>num3)
+{
+printf("num1 is greatest among three ");
+}
+else
+{
+printf("num3 is greatest among three");
+}
+}
+elseif(num2>num3)
+{
+printf("num2 is greatest among three");
+}
+else
+printf(num3 is greatest among three");
+}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
