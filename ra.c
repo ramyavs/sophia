6f834e1828de2f732eca3e4cf14520a74214be7a
ramyavs <31505005+ramyavs@users.noreply.github.com>
2018-01-18 08:09:50 -0300

    Update ra.c

--- ra.c
+++ ra.c
@@ -4,7 +4,7 @@ int main()
 char c;
 printf("enter the character");
 scanf("%d",&c);
-if((c>='a' && c<='z')||(c>='A' && c<='Z')
+if((c>='a' && c<='z')||(c>='A' && c<='Z'))
    {   
 if(c=='a' || c=='A'|| c=='e' || c=='E' || c=='i' || c=='I' || c=='O' || c=='O' || C=='u' || c=='U')
 printf("the character is vowel");
