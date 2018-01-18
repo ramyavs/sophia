597a60caf4b4dfecb9d4d09b4405e777ffdb0295
ramyavs <31505005+ramyavs@users.noreply.github.com>
2018-01-18 08:06:14 -0300

    Update ra.c

--- ra.c
+++ ra.c
@@ -4,9 +4,14 @@ int main()
 char c;
 printf("enter the character");
 scanf("%d",&c);
-if((c>='a' && c<='z')||(c>='A' && c<='Z'))
-printf("the character is aplhabet");
+if((c>='a' && c<='z')||(c>='A' && c<='Z')
+   {   
+if(c=='a' || c=='A'|| c=='e' || c=='E' || c=='i' || c=='I' || c=='O' || c=='O' || C=='u' || c=='U')
+printf("the character is vowel");
 else
-printf("the character is not an alphabet");
+printf("the character is consonent");
+   }
+else 
+printf("the character is neither vowel or consonent");
 return 0;
-}
+   }
