423f0ed159ce8ed1d7546e90aa705d196762362f
ramyavs <31505005+ramyavs@users.noreply.github.com>
2018-01-23 11:19:53 +0000

    Create siri.c

---
+++ siri.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int i ,num,f=1;
+printf("enter the number");
+scanf("%d",&num);
+for(i=1;i<=num;i++)
+f=f*i
+printf("the factorial num %d is:%d",num f);
+return 0;}
