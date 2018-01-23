4a5193c4d49a16181aba116302bb54b1fe60e7bb
ramyavs <31505005+ramyavs@users.noreply.github.com>
2018-01-23 11:25:26 +0000

    Create diya.c

---
+++ diya.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int n,sum=0,i=1;
+printf("enter the value of n");
+scanf("%d",&n);
+while(i<=n)
+{
+if(i%2==1)
+}
+printf("the odd numbers between two intervals %d is:%d",n ,sum);
+}
