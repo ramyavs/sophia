ed145813c36cb1c52186463f7b6d94fc6bef253b
ramyavs <31505005+ramyavs@users.noreply.github.com>
2018-01-26 14:14:03 +0530

    Create ibms.c

---
+++ ibms.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int a=6,b=6;
+char(ch);
+scanf("%c",&ch);
+switch(ch)
+{
+case'+':
+printf("the addition is %d",a+b);
+break;
+case'-':
+printf("the  subtraction is %d ",a-b);
+break;
+case'*':
+printf("the  multiplication is %d",a*b);
+break;
+default:
+printf(" the condition is default");
+} 
+return 0;
+}
+
