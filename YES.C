5c1084181af33b7c18766594d768d3e883eec335
ramyavs <31505005+ramyavs@users.noreply.github.com>
2018-01-26 16:56:19 +0530

    Create YES.C

---
+++ YES.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+int n,i,count=0;
+printf("enter the number");
+scanf("%d",&n);
+for(i=1;i<=10;++i)
+{
+if(i==n)
+{
+count++;
+}}
+if(count==1)
+{
+printf ("%d",yes);
+}
+else
+{
+printf("%d",NO);
+}
+return 0;
+}
+	
+	
+
