c89ca717ffb66beaac38ffbadff9cd915d7bcd31
ramyavs <31505005+ramyavs@users.noreply.github.com>
2018-02-06 15:03:09 +0530

    Create sod.c

---
+++ sod.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+int n,sum=0,rem;
+printf("enter the digit");
+scanf("%d",&n);
+while(n!=0)
+{
+	rem= n % 10;
+	sum=sum + rem;
+n=n/10;
+	
+ printf("Sum of digits of %d = %d\n", n, sum);
+
+}
+return 0;
+}
