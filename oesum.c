d744bcb9a57c14204ae1364cf3f8587c4adacf89
ramyavs <31505005+ramyavs@users.noreply.github.com>
2018-02-09 14:36:07 +0530

    Create oesum.c

---
+++ oesum.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+int n,i,evensum=0,oddsum=0;
+printf("enter the number");
+scanf("%d",&n);
+for(i=1;i<=1;i++)
+{
+if(i%2==0)
+{
+evensum=evensum+i;
+	
+}
+else
+{
+oddsum=oddsum+i;
+}
+printf("the number is even",oddsum);
+	
+}
+printf("the number is odd",evensum);
+return  0;
+}
+
+
