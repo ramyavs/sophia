31e3bf5c0feb479cd945a490f18afe17801a3cf5
ramyavs <31505005+ramyavs@users.noreply.github.com>
2018-02-13 15:04:14 +0530

    Create sumodd.c

---
+++ sumodd.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+  int main() 
+  {
+  int a,b,s;
+  printf("enter the numbers");
+  scanf("%d%d",&a,&b);
+  s=a+b;
+  printf("the sum of two number is:%d",s);
+  if( s %2==0)
+  {
+  	printf("the number is even");
+  	
+  }
+  	else 
+  	{
+  		printf("the number is odd");
+  	}
+ 
+  return  0;
+  }
+  
+  	
