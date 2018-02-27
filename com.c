962e1afcf462ffc4ed76965a61fbd863d2eea523
ramyavs <31505005+ramyavs@users.noreply.github.com>
2018-02-27 15:46:19 +0530

    Create com.c

---
+++ com.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+	int n,i,f;
+	scanf("%d",&n);
+	for(i=1;i<n;i++)
+	{
+		if(n%i==0)
+		{
+		f=i;
+		}}
+		
+		if(f>1)
+		{
+			printf("the number is composite");
+		}
+		else
+		{
+			printf("the number is not a composite");
+		}
+	return  0;
+}
+
