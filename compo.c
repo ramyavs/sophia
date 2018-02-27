40f5bd1b8fde41cfe5805b20c46859bccbb86530
ramyavs <31505005+ramyavs@users.noreply.github.com>
2018-02-27 15:49:19 +0530

    Create compo.c

---
+++ compo.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+	int n,i,f;
+	scanf("%d",&n);
+	for(i=1;i<n;i++)
+	{
+		if(n%i==0)
+		{
+		f=i;
+		}}
+		
+		if(f>1)
+		{
+			printf("yes");
+		}
+		else
+		{
+			printf("no");
+		
+		}
+	return  0;
+}
+
