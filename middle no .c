c1d8a278dfcec5b47e19cacd5712a6e024c967c7
ramyavs <31505005+ramyavs@users.noreply.github.com>
2018-02-27 16:13:10 +0530

    Create middle no .c

---
+++ middle no .c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	int n,l,r;
+	scanf("%d%d%d",&n,&l,&r);
+	if((n>=l)&&(n<=r))
+	{
+		printf("this  is middle no");
+	}
+	else
+	{
+		printf("this is not middle no");
+	}
+return  0;
+}
