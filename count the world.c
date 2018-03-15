31f18f2371896bb3e3028d1278e7eb9ae72e4204
ramyavs <31505005+ramyavs@users.noreply.github.com>
2018-03-15 16:17:48 +0530

    Create count the world.c

---
+++ count the world.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	char a[78];
+	int c=0,i;
+	scanf("%s",&a);
+	for(i=0;a[i]!='\0';i++)
+	{
+		if(a[i]==' ')
+		c++;
+		i++;
+	}
+	printf("%d",ct+1);
+	return 0;
+}
+
+			
